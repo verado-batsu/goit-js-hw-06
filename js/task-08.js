4713079a65680c4b7e28fbb9c7587a3382272c9a
Vladyslav Kupchuk <kupchuk.vlad@gmail.com>
2022-12-21 15:06:17 +0200

    task 8 completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const formEl = document.querySelector('.login-form');
+const arrayOfInputEl = document.querySelectorAll('.login-form input');
 
+
+const btnFormEl = document.querySelector('.login-form button');
+
+
+formEl.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+	event.preventDefault();
+	const { elements: { email, password } } = event.currentTarget;
+
+	if (email.value.trim() === '' || password.value.trim() === '') {
+		alert("Всі поля повинні бути заповнені!");
+	};
+
+	const userData = {
+		[email.name]: email.value.trim(),
+		[password.name]: password.value.trim(),
+	};
+
+	console.log(userData);
+	event.currentTarget.reset();
+}
