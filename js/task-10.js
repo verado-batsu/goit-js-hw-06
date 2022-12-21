5f36ee04d1af81b7ec79cbc6d55985b2064141a6
Vladyslav Kupchuk <kupchuk.vlad@gmail.com>
2022-12-21 16:06:54 +0200

    task 10 completed

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,41 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const controlsInputEl = document.querySelector("#controls > input");
+
+const createBtnEl = document.querySelector("[data-create]");
+const destroyBtnEl = document.querySelector("[data-destroy]");
+
+const boxesEl = document.querySelector("#boxes");
+
+
+createBtnEl.addEventListener('click', onClickCreate);
+destroyBtnEl.addEventListener('click', destroyBoxes);
+
+function onClickCreate() {
+	destroyBoxes();
+	let amount = 0;
+	amount = controlsInputEl.value;
+	createBoxes(amount);
+}
+
+
+function createBoxes(amount) {
+	let div = '';
+	let size = 30;
+
+	for (let i = 1; i <= amount; i += 1){
+		div += `<div style="width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()}"></div>`;
+		size += 10;
+	}
+
+	boxesEl.insertAdjacentHTML("beforeend", div);
+}
+
+function destroyBoxes() {
+	boxesEl.innerHTML = '';
+}
+
+
+
